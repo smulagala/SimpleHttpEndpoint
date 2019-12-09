74b62f2b9dea441b5e434a621d743b014f9874c3
Mulagala <python.shiva@gmail.com>
2019-12-09 15:41:28 +0530

    Initial commit

---
+++ handler.py
@@ -0,0 +1,44 @@
+import json
+import datetime
+
+def hello(event, context):
+    body = {
+        "message": "The current date and time is: "+ str(datetime.datetime.now())
+    }
+
+    response = {
+        "statusCode": 200,
+        "body": json.dumps(body)
+    }
+
+    return response
+
+    # Use this code if you don't use the http event with the LAMBDA-PROXY
+    # integration
+    """
+    return {
+        "message": "Go Serverless v1.0! Your function executed successfully!",
+        "event": event
+    }
+    """
+
+def hello2(event, context):
+    body = {
+        "message": "The current date and time is from second func: "+ str(datetime.datetime.now())
+    }
+
+    response = {
+        "statusCode": 200,
+        "body": json.dumps(body)
+    }
+
+    return response
+
+    # Use this code if you don't use the http event with the LAMBDA-PROXY
+    # integration
+    """
+    return {
+        "message": "Go Serverless v1.0! Your function executed successfully!",
+        "event": event
+    }
+    """
