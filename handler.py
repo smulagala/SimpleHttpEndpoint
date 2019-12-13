fc5293b86e54c529d20954ecc055b7cf13207381
Siva Mulagala <siva.mulagala@businesswire.com>
2019-12-13 15:39:01 +0530

    First commit

--- handler.py
+++ handler.py
@@ -1,44 +1,63 @@
 import json
 import datetime
+# import requests
 
-def hello(event, context):
-    body = {
-        "message": "The current date and time is: "+ str(datetime.datetime.now())
-    }
+# def hello(event, context):
+#     body = {
+#         "message": "The current date and time is: "+ str(datetime.datetime.now())
+#     }
 
+#     response = {
+#         "statusCode": 200,
+#         "body": json.dumps(body)
+#     }
+
+#     return response
+
+#     # Use this code if you don't use the http event with the LAMBDA-PROXY
+#     # integration
+#     """
+#     return {
+#         "message": "Go Serverless v1.0! Your function executed successfully!",
+#         "event": event
+#     }
+#     """
+
+# def hello2(event, context):
+#     body = {
+#         "message": "The current date and time is from second func: "+ str(datetime.datetime.now())
+#     }
+
+#     response = {
+#         "statusCode": 200,
+#         "body": json.dumps(body)
+#     }
+
+#     return response
+
+#     # Use this code if you don't use the http event with the LAMBDA-PROXY
+#     # integration
+#     """
+#     return {
+#         "message": "Go Serverless v1.0! Your function executed successfully!",
+#         "event": event
+#     }
+#     """
+
+
+def greet_me(event, context):
+    # TODO implement
+    # name = event['name']
+    body = json.loads(event['body'])
+    # data = {"name": name}
     response = {
-        "statusCode": 200,
+        'statusCode': 200,
+        "headers": { 
+            "Access-Control-Allow-Origin": "*",
+            # 'Content-Type': 'application/json'
+        },
         "body": json.dumps(body)
     }
-
+    
     return response
 
-    # Use this code if you don't use the http event with the LAMBDA-PROXY
-    # integration
-    """
-    return {
-        "message": "Go Serverless v1.0! Your function executed successfully!",
-        "event": event
-    }
-    """
-
-def hello2(event, context):
-    body = {
-        "message": "The current date and time is from second func: "+ str(datetime.datetime.now())
-    }
-
-    response = {
-        "statusCode": 200,
-        "body": json.dumps(body)
-    }
-
-    return response
-
-    # Use this code if you don't use the http event with the LAMBDA-PROXY
-    # integration
-    """
-    return {
-        "message": "Go Serverless v1.0! Your function executed successfully!",
-        "event": event
-    }
-    """
