1376a3580354f214270f9106cbcaa934327e1308
Siva Mulagala <siva.mulagala@businesswire.com>
2019-12-19 11:39:51 +0530

    Added new methods

--- src/handler.py
+++ src/handler.py
@@ -46,7 +46,6 @@ import datetime
 
 
 def greet_me(event, context):
-    # TODO implement
     # name = event['name']
     body = json.loads(event['body'])
     # data = {"name": name}
@@ -61,21 +60,31 @@ def greet_me(event, context):
     
     return response
 
+def get_method(event, context):
+    body = json.loads(event['body'])
+    query_params = event['queryStringParameters']
+    message = {'body': body, 'params': query_params}
+    response = make_response(message)
+    return response
+
+def post_method(event, context):
+    body = json.loads(event['body'])
+    response = make_response(body)
+    return response
+
 def put_method(event, context):
     body = json.loads(event['body'])
-    response = {
-        'statusCode': 200,
-        "headers": {
-            "Access-Control-Allow-Origin": "*"
-        },
-        "body": json.dumps(body)
-    }
+    response = make_response(body)
     return response
 
 def delete_method(event, context):
     body = json.loads(event['body'])
     query_params = event['queryStringParameters']
     message = {'body': body, 'params': query_params}
+    response = make_response(message)
+    return response
+
+def make_response(message):
     response = {
         'statusCode': 200,
         "headers": {
